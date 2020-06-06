e379aed86de0f944f567bff54dce0837604e17f7
Luka Tassano <lukatassano070@gmail.com>
2020-06-05 22:39:11 -0300

    update

--- backend/src/database/connection.ts
+++ backend/src/database/connection.ts
@@ -1,7 +1,7 @@
-import knex from "knex";
+import Knex from "knex";
 import path from "path";
 
-const connection = knex({
+const connection = Knex({
   client: "sqlite3",
   connection: {
     filename: path.resolve(__dirname, "database.sqlite"),
